75c6ab818aebf0dd5bafe2f583b969ff405103f8
end-sen <kadiridhanushka@gmail.com>
2024-11-20 18:19:38 +0530

    Uploading code: Masterchef Finals.c

---
+++ Masterchef Finals.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+    return 0;
+}
